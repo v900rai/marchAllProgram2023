b5e2d3c0afd242d94425efa9d01abcf7fe5c77d6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-25 17:45:11 +0530

    Add files via upload

---
+++ basicProgram.java
@@ -0,0 +1,15 @@
+package stringAllPeogram;
+
+public class basicProgram
+{
+	public static void main(String []args)
+	{
+		String str="hello";
+		int len=str.length();
+		System.out.println("Original "+len);
+		System.out.println();
+		char chr=str.charAt(3);
+		System.out.println(chr);
+	}
+
+}
--- reversString.java
+++ reversString.java
@@ -1,19 +1,18 @@
-package DSA;
+package stringAllPeogram;
 
-public class reversString
+
+public class reversString 
 {
-	public static void main(String []args)
-	{
-		String s1="vishalrai";
-		String rev="";
-		System.out.println("orijanl String" +s1);
-		for(int i=s1.length()-1; i>=0; i--)
-		{
-			rev=rev+s1.charAt(i);
-		}
-		System.out.println("Revers String" +rev);
-		
-		
-	}
-
+  public static void main(String []args)
+  {
+	  String str="Vishalrai";
+	  String rev="";
+	  for(int i=str.length()-1; i>=0; i--)
+	  {
+		  rev=rev+str.charAt(i);
+	  }
+	  System.out.println(str);
+	  System.out.println(rev);
+  }
+	
 }
