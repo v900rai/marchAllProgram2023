0fa39ee81646d36b66ef919cb7132ec2f74a4826
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-25 21:15:54 +0530

    Add files via upload

---
+++ secondMethodStringRevers.java
@@ -0,0 +1,13 @@
+package stringAllPeogram;
+
+public class secondMethodStringRevers 
+{
+	public static void main(String []args)
+	{
+		String str="vishalrai";
+		System.out.println(str);
+		StringBuilder s=new StringBuilder(str);
+		System.out.println(s.reverse());
+	}
+
+}
