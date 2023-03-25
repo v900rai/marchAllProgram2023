d62a51e02caa79265e88458c19077d1fea6d5200
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-25 21:19:20 +0530

    Add files via upload

---
+++ thirdMethodStringRevrsss.java
@@ -0,0 +1,13 @@
+package stringAllPeogram;
+
+public class thirdMethodStringRevrsss
+{
+	public static void main(String []args)
+	{
+		String str="vishalrai";
+		System.out.println(str);
+		StringBuffer s= new StringBuffer(str);
+		System.out.println(s.reverse());
+	}
+
+}
