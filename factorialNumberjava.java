98564e931d70d3f72d166d07be6964f2735441eb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-27 18:57:14 +0530

    Add files via upload

---
+++ factorialNumberjava.java
@@ -0,0 +1,16 @@
+package stringAllPeogram;
+
+public class factorialNumberjava 
+{
+	public static void main(String []args)
+	{
+	int num=6;
+	int fact=1;
+	while(num>=1)
+	{
+		fact=fact*num;
+		num--;
+	}
+	System.out.println(fact);
+	}
+}
