7efdfb197507789db3a6f81982d910e80ec2f9db
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-28 18:31:30 +0530

    Add files via upload

---
+++ IfElseclauseinjava.java
@@ -0,0 +1,15 @@
+package ArraysProgram;
+
+public class IfElseclauseinjava 
+{
+	public static void main(String []args)
+	{
+		boolean learning = true;
+		 if (learning) {
+		 System.out.println("Java programmer");
+		 }
+		 else {
+		 System.out.println("What are you doing here?");
+	}
+	}
+}
