ef901415525204afc7aad17342b0345166cf8b20
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-03-28 18:07:51 +0530

    Add files via upload

---
+++ addTwoNUmber.java
@@ -0,0 +1,16 @@
+package ArraysProgram;
+import java.util.Scanner;
+public class addTwoNUmber 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		
+	int num3=num1+num2;
+	System.out.println("Add number :"+num3);
+		
+	}
+
+}
